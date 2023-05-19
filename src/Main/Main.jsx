628167699e242d8649ee22bd3b82a7b3d4ddea32
sohelranagtc <sohelranagtc@gmail.com>
2023-05-19 10:21:37 +0600

    add footer

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom'
 import Header from '../Components/Shared/Header/Header';
+import Footer from '../Components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
+
         </div>
     );
 };
